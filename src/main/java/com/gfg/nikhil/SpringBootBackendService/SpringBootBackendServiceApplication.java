f7db9d58027ecff4f5ddbc51cce822c5e4a7d28e
Nikhil <nikhil.srivastava944@gmail.com>
2019-06-20 23:06:26 +0530

    Downloaded skeleton from Spring Initializr

---
+++ src/main/java/com/gfg/nikhil/SpringBootBackendService/SpringBootBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gfg.nikhil.SpringBootBackendService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gfg/nikhil/SpringBootBackendService/SpringBootBackendServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gfg.nikhil.SpringBootBackendService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootBackendServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
