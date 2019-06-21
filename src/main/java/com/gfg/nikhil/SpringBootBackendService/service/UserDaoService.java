718bdef6e161c94ac4dc22f9a99369ab59c30e3e
Nikhil Srivastava <nikhil.srivastava@mtree.co.in>
2019-06-21 12:01:44 +0530

    creates UserService

---
+++ src/main/java/com/gfg/nikhil/SpringBootBackendService/service/UserDaoService.java
@@ -0,0 +1,48 @@
+package com.gfg.nikhil.SpringBootBackendService.service;
+
+import com.gfg.nikhil.SpringBootBackendService.model.User;
+import com.gfg.nikhil.SpringBootBackendService.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserDaoService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAllUsers(){
+        List<User> users=new ArrayList<>();
+
+        users.add((User) userRepository.findAll());
+        return  users;
+    }
+
+    public Optional<User> findUserById(Long id){
+
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user){
+
+        userRepository.save(user);
+        return user;
+    }
+    public User updateUser(User user){
+
+        user.setFirstName(user.getFirstName());
+        user.setLastName(user.getLastName());
+        userRepository.save(user);
+        return user;
+    }
+
+    public String deleteUser(Long id){
+
+        userRepository.deleteById(id);
+        return "Deleted user with id "+id;
+    }
+}
